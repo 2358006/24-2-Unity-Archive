d9be7742377a144631862e6fdca02db46d0106f3
mw2358006 <2358006@mokwon.ac.kr>
2024-09-06 13:39:07 +0900

    create MoveBall.cs

---
+++ Assets/Script/MoveBall.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("정상 작동");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
