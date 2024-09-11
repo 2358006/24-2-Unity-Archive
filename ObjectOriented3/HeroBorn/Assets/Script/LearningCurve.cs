28ccb6eb008a03d8c99915caaeb960ab7b0d2180
mw2358006 <2358006@mokwon.ac.kr>
2024-09-11 10:33:38 +0900

    객지3 실습파일

---
+++ ObjectOriented3/HeroBorn/Assets/Script/LearningCurve.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class LearningCurve : MonoBehaviour
+{
+    public int currentAge = 30;
+    public int addAge = 1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(30 + 1);
+        Debug.Log(currentAge + 1);
+
+        // ComputerAge();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /*
+    public void ComputerAge()
+    {
+       Debug.Log(currentAge + addAge);
+    }
+    */
+}
