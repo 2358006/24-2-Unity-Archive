93013a1372afa90e26e8b3d8cce62780bf12100c
mw2358006 <2358006@mokwon.ac.kr>
2024-09-12 13:22:59 +0900

    Update LearningCurve.cs

--- ObjectOriented3/HeroBorn/Assets/Script/LearningCurve.cs
+++ ObjectOriented3/HeroBorn/Assets/Script/LearningCurve.cs
@@ -5,27 +5,30 @@ public class LearningCurve : MonoBehaviour
     public int currentAge = 30;
     public int addAge = 1;
 
+    public float pi = 3.14f
+    public string firstName = "Harrison";
+    public bool isAuthor = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        // Debug.Log(30 + 1);
-        // Debug.Log(currentAge + 1);
-
         ComputerAge();
+        Debug.Log($"A string can have variables like {firstName} inserted directly");
+        Debug.LogFormat("A string can have variables like {0} inserted directly", fileName);
+
+    }
+
+    /// <summary>
+    /// 주석 추가 : CurrentAge와 AddedAge의 합을 출력한다.
+    /// </summary>
+    public void ComputerAge()
+    {
+    Debug.Log(currentAge + addAge);
     }
 
     // Update is called once per frame
     void Update()
     {
 
-    }
-
-/// <summary>
-    /// 주석 추가 : CurrentAge와 AddedAge의 합을 출력한다.
-    /// </summary>
-    public void ComputerAge()
-    {
-    Debug.Log(currentAge + addAge);
     }
 }
