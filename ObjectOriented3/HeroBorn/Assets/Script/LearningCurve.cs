65f52e18157c6f2664621652253ab420d81069f2
mw2358006 <2358006@mokwon.ac.kr>
2024-09-12 14:21:53 +0900

    Update LearningCurve.cs

--- ObjectOriented3/HeroBorn/Assets/Script/LearningCurve.cs
+++ ObjectOriented3/HeroBorn/Assets/Script/LearningCurve.cs
@@ -1,3 +1,4 @@
+using System.Data;
 using UnityEngine;
 
 public class LearningCurve : MonoBehaviour
@@ -12,9 +13,17 @@ public class LearningCurve : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        ComputerAge();
-        Debug.Log($"A string can have variables like {firstName} inserted directly");   
-        Debug.LogFormat("A string can have variables like {0} inserted directly", firstName);
+        int characterLevel = 10;
+        int nextSkillLevel = GenerateCharacter("Spike", characterLevel);
+        // ComputerAge();
+
+        // Debug.LogFormat("A string can have variables like {0} inserted directly", firstName);
+        // Debug.Log($"A string can have variables like {firstName} inserted directly");   
+        
+        Debug.Log(nextSkillLevel);
+        Debug.Log("Choose a character");
+        GenerateCharacter("Spike", characterLevel);
+        Debug.Log("A fine choice.");
     }
 
     /// <summary>
@@ -22,8 +31,26 @@ public class LearningCurve : MonoBehaviour
     /// </summary>
     public void ComputerAge()
     {
-    Debug.Log(currentAge + addAge);
+        Debug.Log(currentAge + addAge);
     }
+
+/*
+    public void GenerateCharacter(string name, int level)
+    {
+        // Debug.Log("Character : Spike");
+        Debug.LogFormat("character : {0} - Level : {1}", name, level);
+    }
+*/
+
+public int GenerateCharacter(string name, int level)
+    {
+        // Debug.Log("Character : Spike");
+        Debug.LogFormat("character : {0} - Level : {1}", name, level);
+
+        return level += 5;
+    }
+
+
 
     // Update is called once per frame
     void Update()
