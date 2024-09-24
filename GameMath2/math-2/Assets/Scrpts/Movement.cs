be8ec06a1812e94457d99abdda6e68a9d6ae28e1
mw2358006 <2358006@mokwon.ac.kr>
2024-09-24 11:30:45 +0900

    scripting

---
+++ GameMath2/math-2/Assets/Scrpts/Movement.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private void Awake()
+    {
+        Vector2 vector2 = new Vector2(); 
+
+        vector2.x = 1;
+        vector2.y = 1;
+    }
+}
