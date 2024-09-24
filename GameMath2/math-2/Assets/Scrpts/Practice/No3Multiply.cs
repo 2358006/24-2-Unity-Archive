4e83267d96b061baaa18727e68a0fe59e4655cbb
mw2358006 <2358006@mokwon.ac.kr>
2024-09-24 12:10:37 +0900

    name change

---
+++ GameMath2/math-2/Assets/Scrpts/Practice/No3Multiply.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class No3Multiply : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 3. Vector와 스칼라의 곱셈
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
