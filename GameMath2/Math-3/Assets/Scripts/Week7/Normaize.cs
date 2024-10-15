9ca380a6da6681e3fb15a884ee9dfbcf11b04980
mw2358006 <2358006@mokwon.ac.kr>
2024-10-15 10:26:18 +0900

    7주차 수업

---
+++ GameMath2/Math-3/Assets/Scripts/Week7/Normaize.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Normaize : MonoBehaviour
+{
+    /// <summary>
+    ///  5. 벡터의 정규화
+    ///  Vector3 구조체의 normalized 프로퍼티를 이용하면
+    ///  정규화된 단위벡터를 가져올 수 있다.
+    /// </summary>
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
