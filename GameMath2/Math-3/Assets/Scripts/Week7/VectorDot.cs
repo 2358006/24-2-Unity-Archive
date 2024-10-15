b0a525748cd579e4fa5223d1b520a53ad3c23979
mw2358006 <2358006@mokwon.ac.kr>
2024-10-15 11:28:34 +0900

    Update VectorDot.cs

--- GameMath2/Math-3/Assets/Scripts/Week7/VectorDot.cs
+++ GameMath2/Math-3/Assets/Scripts/Week7/VectorDot.cs
@@ -26,6 +26,18 @@ public class VectorDot : MonoBehaviour
         Debug.Log("Angle Degree of v1, v90 = " + Vector3.Angle(v1, v90));
         Debug.Log("Angle Degree of v1, v135 = " + Vector3.Angle(v1, v135));
 
+        ///<summary>
+        /// 벡터의 내적에 대해서 설명했듯,
+        /// v1과 v45 사이에 각도는 90도 보다 작기 때문에
+        /// 0보다 큰 값이 나왔고,
+        /// v1과 v90 사이 각은 정확히 90도이기 때문에 0
+        /// v1과 v135 사이의 각은 90도 보다 커서 
+        /// 0보다 작은 값이 나온 것을 확인 할 수 있다.
+        ///
+        /// vector3의 또다른 함수인 Angle 함수를 사용하면
+        /// 두 벡터 사이의 각을 얻을 수 있다. 
+        ///</summary>
+
     }
 
     // Update is called once per frame
