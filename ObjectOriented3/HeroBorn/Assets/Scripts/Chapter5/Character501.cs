d24fd9d4887b15801c8c9f21a7d09b1cb87a7767
mw2358006 <2358006@mokwon.ac.kr>
2024-10-16 11:31:48 +0900

    파일정리

---
+++ ObjectOriented3/HeroBorn/Assets/Scripts/Chapter5/Character501.cs
@@ -0,0 +1,15 @@
+public class Character501
+{
+    public string name;
+    public int exp = 0;
+
+    public Character501()
+    {
+        name = "Not assigned";
+    }
+
+        public Character501(string name)
+    {
+        this.name = name;
+    }
+}
