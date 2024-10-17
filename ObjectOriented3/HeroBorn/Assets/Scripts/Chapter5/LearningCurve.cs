9607f919c8447f51e034ec46c951075f27dc2363
mw2358006 <2358006@mokwon.ac.kr>
2024-10-17 21:12:09 +0900

    c5 실습 시작

---
+++ ObjectOriented3/HeroBorn/Assets/Scripts/Chapter5/LearningCurve.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearningCurve : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Character hero = new Character();
+        hero.PrintStatsInfo();
+
+        Character heroine = new Character("Agatha");
+        heroine.PrintStatsInfo();
+    }
+}
