45218f97957ec80d614b0c96d9cd45a5a36af7ef
mw2358006 <2358006@mokwon.ac.kr>
2024-10-17 23:05:05 +0900

    511 정상화

--- ObjectOriented3/HeroBorn/Assets/Scripts/Chapter5/510/LearningCurve510.cs
+++ ObjectOriented3/HeroBorn/Assets/Scripts/Chapter5/510/LearningCurve510.cs
@@ -5,7 +5,7 @@ public class LearningCurve510 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {   
-        Paladin knight = new Paladin("Sir Arthur");
+        Paladin510 knight = new Paladin510("Sir Arthur");
         knight.PrintStatsInfo();
     }
 }
