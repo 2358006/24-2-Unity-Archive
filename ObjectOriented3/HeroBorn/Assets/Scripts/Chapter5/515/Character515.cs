c59276357556b88e5abff418278f72d803fab2c8
mw2358006 <2358006@mokwon.ac.kr>
2024-10-17 23:47:25 +0900

    5실습 끝

---
+++ ObjectOriented3/HeroBorn/Assets/Scripts/Chapter5/515/Character515.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+public class Character515
+{
+    public string name;
+    public int exp = 0; 
+
+    public Character515()
+    {
+    }
+    public Character515(string name)
+    {
+        this.name = name;
+    }
+    public virtual void PrintStatsInfo()
+    {
+        Debug.LogFormat("Hero : {0} - {1} EXP", this.name, this.exp);
+    }
+}
+public class Paladin : Character515
+{
+    public Weapon515 weapon;
+    public Paladin515(string name, Weapon515 weapon) : base(name)
+    {
+        this.weapon = weapon;
+    }            
+    public override void PrintStatsInfo()
+    {
+        Debug.LogFormat("Hail {0} - take up your {1}!", this.name, this.weapon.name);
+    }
+
+}
