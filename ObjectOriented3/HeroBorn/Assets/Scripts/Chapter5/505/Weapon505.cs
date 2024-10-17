1a7a368f26c63454a4f40f8aab1adb681c3fac78
mw2358006 <2358006@mokwon.ac.kr>
2024-10-17 21:34:43 +0900

    505정상화

---
+++ ObjectOriented3/HeroBorn/Assets/Scripts/Chapter5/505/Weapon505.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public struct Weapon505
+{
+    public string name;
+    public int damage;
+
+    public Weapon505(string name, int damage)
+    {
+        this.name = name;
+        this.damage = damage;
+    }
+    public void PrintWeaponStats()
+    {
+        Debug.LogFormat("Weapon : {0} - {1} DMG", this.name, this.damage);
+    }
+}
